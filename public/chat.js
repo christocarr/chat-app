4ed8a834778c3b24caf5cbeab36ce059929da12c
Chris Carr <christocarr@outlook.com>
2019-12-05 13:07:07 +0000

    Add send button functionality

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,17 @@
 //make connection
 const socket = io.connect('http://localhost:3000')
+
+//get DOM elements
+const output = document.getElementById('output')
+const name = document.getElementById('name')
+const message = document.getElementById('message')
+const send = document.getElementById('send')
+
+//emit events
+send.addEventListener('click', () => {
+  socket.emit('chat', {
+    message: message.nodeValue,
+    name: name.value
+  })
+})
+
